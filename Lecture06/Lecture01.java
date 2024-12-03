c5adfb29a3df10dd1b17a0e698806d42c19615d3
Adeonthebeat <fowsdw@naver.com>
2024-12-03 21:51:53 +0900

    헤에

--- Lecture06/Lecture01.java
+++ Lecture06/Lecture01.java
@@ -1,9 +1,41 @@
 package studyForMe.Lecture06;
 
+import java.util.Scanner;
+
 public class Lecture01 {
 
-    public static void main(String[] args) {
+    public int[] solution(int n, int[] arr) {
 
+       for(int i = 0; i < n-1; i++) {
+           int idx = i;
+           for(int j = i+1; j < n; j++) {
+               if(arr[j] < arr[idx]) {
+                   idx = j;
+               }
+           }
+           int temp = arr[i];
+           arr[i] = arr[idx];
+           arr[idx] = temp;
+       }
+
+        return arr;
     }
 
+    public static void main(String[] args) {
+        Lecture01 main = new Lecture01();
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        for(int x : main.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+
+    }
 }
